70635dc914f76accdb116db858045503c0da7921
djk01281 <djk01281@gmail.com>
2023-04-27 11:31:23 +0000

    feat: refresh page after submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
+
 function App() {
 
   const [backendData, setBackendData] = useState([{}])
+  const [memo, setMemo] = useState("")
 
   useEffect( ()=>{
     const fetchData = async () => {
@@ -13,8 +15,41 @@ function App() {
     fetchData()
   }, [])
 
+  const submitHandler = async (e) =>{
+    e.preventDefault()
+    const url = "/api/create";
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        text: memo
+      }),
+    };
+    const response = await fetch(url, options)
+    console.log(response)
+    const fetchData = async () =>{
+      const data = await fetch('/api/all')
+      const jsonData = await data.json()
+      setBackendData(jsonData)
+    }
+    await fetchData()
+  }
+
+  const memoHandler = (e) =>{
+    e.preventDefault()
+    setMemo(e.target.value)
+  }
+
   return (
       <div>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={memo} onChange={memoHandler}></input>
+          <button type="submit">Add Memo</button>
+        </form>
+        <div>
         {(typeof backendData.memos === 'undefined')?(
           <p>Loading</p>
         ):(
@@ -22,6 +57,7 @@ function App() {
             return <p>{memo.text}</p>
           })
         )}
+        </div>
     </div>
   )
 }
